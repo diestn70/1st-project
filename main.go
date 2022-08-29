ab1c5725968b5f0f22724c89d7b9c7873a542204
diestn70 <111007805+diestn70@users.noreply.github.com>
2022-08-29 13:32:34 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -5,21 +5,21 @@ import (
 	"os"
 )
 
-// ---------------------------------------------------------
-// EXERCISE: Print Your Fullname
-//
-//  1. Get your name and lastname from the command-line
-//  2. Print them using Printf
-//
-// EXAMPLE INPUT
-//  Inanc Gumus
-//
-// EXPECTED OUTPUT
-//  Your name is Inanc and your lastname is Gumus.
-// ---------------------------------------------------------
-
 func main() {
-	name, lastname := os.Args[1], os.Args[2]
-	msg :="Your name is %s and your last name is %s"
-	fmt.Printf(msg,name,lastname)
+	args := os.Args
+
+	if len(args) != 3 {
+		fmt.Println("Usage: [username] [password]")
+		return
+	}
+
+	u, p := args[1], args[2]
+
+	if u != "jack" {
+		fmt.Printf("Access denied for %q.\n", u)
+	} else if p != "1888" {
+		fmt.Printf("Invalid password for %q.\n", u)
+	} else {
+		fmt.Printf("Access granted to %q.\n", u)
+	}
 }
